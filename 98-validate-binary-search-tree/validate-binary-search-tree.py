36ed89c5ad8706edff29cad8c353b730b0a306c8
lakshh670 <er.lakshyasharma2005@gmail.com>
2026-07-06 09:23:49 +0530

    Time: 35 ms (3.81%) | Memory: 22 MB (5.48%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.py
+++ 98-validate-binary-search-tree/validate-binary-search-tree.py
@@ -8,23 +8,32 @@ class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
         if root is None:
             return True
+        # My approach
+        # def greatest_left_tree(temp):
+        #     if temp is None:
+        #         return float('-inf')
+        #     while temp.right:
+        #         temp=temp.right
+        #     return temp.val
 
-        def greatest_left_tree(temp):
-            if temp is None:
-                return float('-inf')
-            while temp.right:
-                temp=temp.right
-            return temp.val
+        # def smallest_right_tree(temp):
+        #     if temp is None:
+        #         return float('inf')
+        #     while temp.left:
+        #         temp=temp.left
+        #     return temp.val
 
-        def smallest_right_tree(temp):
-            if temp is None:
-                return float('inf')
-            while temp.left:
-                temp=temp.left
-            return temp.val
+        # Most optimal
+        def dfs(node,low,high):
+            if node is None:
+                return True
+            if not low<node.val<high:
+                return False
+            return dfs(node.left,low,node.val) and dfs(node.right,node.val,high)
+
 
         case1=self.isValidBST(root.left) 
         case2= self.isValidBST(root.right)
-        case3=greatest_left_tree(root.left)<root.val and smallest_right_tree(root.right)>root.val
+        case3=dfs(root,float('-inf'),float('inf'))
         return case1 and case2 and case3
         
