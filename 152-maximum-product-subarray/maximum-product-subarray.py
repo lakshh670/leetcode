9729b680cd58bafeb714269ac0ec0263a6656ad5
lakshh670 <er.lakshyasharma2005@gmail.com>
2026-07-15 10:29:00 +0530

    Time: 3 ms (91.07%) | Memory: 19.7 MB (97.26%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.py
+++ 152-maximum-product-subarray/maximum-product-subarray.py
@@ -19,23 +19,34 @@ class Solution:
         # # Return the result
         # return maxProd
 
-        # Approach2:
-        maxi1=float('-inf')
-        prod=1
-        for x in nums:
-            prod*=x
-            maxi1=max(prod,maxi1)
-            if prod==0:
-                prod=1
+        # Approach2: we just need to apply kadane's algo twice, one in forward direction and other in backward direction.
+        # maxi1=float('-inf')
+        # prod=1
+        # for x in nums:
+        #     prod*=x
+        #     maxi1=max(prod,maxi1)
+        #     if prod==0:
+        #         prod=1
             
-        maxi2=float('-inf')
-        prod=1
-        for x in nums[::-1]:
+        # maxi2=float('-inf')
+        # prod=1
+        # for x in nums[::-1]:
             
-            prod*=x
-            maxi2=max(prod,maxi2)
-            if prod==0:
-                prod=1
+        #     prod*=x
+        #     maxi2=max(prod,maxi2)
+        #     if prod==0:
+        #         prod=1
             
-        return max(maxi1,maxi2)
+        # return max(maxi1,maxi2)
+
+
+        # Approach3:
+        res,max_prod,min_prod=nums[0],nums[0],nums[0]
+        for x in nums[1:]:
+            if x<0:
+                max_prod,min_prod=min_prod,max_prod
+            max_prod=max(x,max_prod*x)
+            min_prod=min(x,min_prod*x)
+            res=max(res,max_prod)
+        return res
             
