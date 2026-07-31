a5fb38a17ef5fc45db06fb991ec4a7d0ddc91ae2
lakshh670 <er.lakshyasharma2005@gmail.com>
2026-08-01 01:44:19 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (79.02%) - LeetSync

--- 162-find-peak-element/find-peak-element.py
+++ 162-find-peak-element/find-peak-element.py
@@ -1,14 +1,24 @@
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
-        if len(nums)==1:
-            return 0
-        nums.append(float('-inf'))
-        i=1
-        while i<len(nums)-1:
-            if nums[i-1]<nums[i]>nums[i+1]:
-                return i
-            i+=1
-        return 0
-        
+        # Brute Force
+        # if len(nums)==1:
+        #     return 0
+        # nums.append(float('-inf'))
+        # i=1
+        # while i<len(nums)-1:
+        #     if nums[i-1]<nums[i]>nums[i+1]:
+        #         return i
+        #     i+=1
+        # return 0
+
+        # Modified binary search
+        left,right=0,len(nums)-1
+        while left<right:
+            mid=left+(right-left)//2
+            if nums[mid]>nums[mid+1]:
+                right=mid
+            else:
+                left=mid+1
+        return left
 
         
