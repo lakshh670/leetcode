2e9f2c2d0f2c67e0756737b6b81865dec6cddd3b
lakshh670 <er.lakshyasharma2005@gmail.com>
2026-08-12 20:40:42 +0530

    Time: 58 ms (21.12%) | Memory: 19.4 MB (72.71%) - LeetSync

--- 97-interleaving-string/interleaving-string.py
+++ 97-interleaving-string/interleaving-string.py
@@ -4,23 +4,40 @@ class Solution:
         l1,l2,l3=len(s1),len(s2),len(s3)
         if l3!=l1+l2:
             return False
-        dp=[[-1]*l2 for _ in range(l1)] 
-        def solve(i,j):
-            if i==l1:
-                return s3[i+j:]==s2[j:]
-            if j==l2:
-                return s3[i+j:]==s1[i:]
-            if dp[i][j]!=-1:
-                return dp[i][j]
-            option1,option2=False,False
-            if s1[i]==s3[i+j]:
+        dp=[[-1]*(l2+1) for _ in range(l1+1)] 
+        # def solve(i,j):
+        #     if i==l1:
+        #         return s3[i+j:]==s2[j:]
+        #     if j==l2:
+        #         return s3[i+j:]==s1[i:]
+        #     if dp[i][j]!=-1:
+        #         return dp[i][j]
+        #     option1,option2=False,False
+        #     if s1[i]==s3[i+j]:
                 
-                option1=solve(i+1,j)
-            if s2[j]==s3[i+j]:    
+        #         option1=solve(i+1,j)
+        #     if s2[j]==s3[i+j]:    
 
-                option2=solve(i,j+1)
-            dp[i][j]= option1 or option2
-            return dp[i][j]
-        return solve(0,0)
+        #         option2=solve(i,j+1)
+        #     dp[i][j]= option1 or option2
+        #     return dp[i][j]
+
+        # return solve(0,0)
+
+        for j in range(l2):
+            dp[l1][j]=s3[l1+j:]==s2[j:]
+        for i in range(l1):
+            dp[i][l2]=s3[i+l2:]==s1[i:]
+        for i in range(l1-1,-1,-1):
+            for j in range(l2-1,-1,-1):
+                option1,option2=False,False
+                if s1[i]==s3[i+j]:
+                
+                    option1=dp[i+1][j]
+                if s2[j]==s3[i+j]:    
+
+                    option2=dp[i][j+1]
+                dp[i][j]= option1 or option2
+        return dp[0][0]
             
         
