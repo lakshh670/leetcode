2a9f7664fe6724268422da3238c56f1da574b7c1
lakshh670 <er.lakshyasharma2005@gmail.com>
2026-08-12 20:31:26 +0530

    Time: 55 ms (35.77%) | Memory: 20 MB (30.76%) - LeetSync

--- 97-interleaving-string/interleaving-string.py
+++ 97-interleaving-string/interleaving-string.py
@@ -4,23 +4,23 @@ class Solution:
         l1,l2,l3=len(s1),len(s2),len(s3)
         if l3!=l1+l2:
             return False
-        dp=[[[-1]*l3 for _ in range(l2)] for _ in range(l1)]
-        def solve(i,j,k):
+        dp=[[-1]*l2 for _ in range(l1)] 
+        def solve(i,j):
             if i==l1:
-                return s3[k:]==s2[j:]
+                return s3[i+j:]==s2[j:]
             if j==l2:
-                return s3[k:]==s1[i:]
-            if dp[i][j][k]!=-1:
-                return dp[i][j][k]
+                return s3[i+j:]==s1[i:]
+            if dp[i][j]!=-1:
+                return dp[i][j]
             option1,option2=False,False
-            if s1[i]==s3[k]:
+            if s1[i]==s3[i+j]:
                 
-                option1=solve(i+1,j,k+1)
-            if s2[j]==s3[k]:    
+                option1=solve(i+1,j)
+            if s2[j]==s3[i+j]:    
 
-                option2=solve(i,j+1,k+1)
-            dp[i][j][k]= option1 or option2
-            return dp[i][j][k]
-        return solve(0,0,0)
+                option2=solve(i,j+1)
+            dp[i][j]= option1 or option2
+            return dp[i][j]
+        return solve(0,0)
             
         
