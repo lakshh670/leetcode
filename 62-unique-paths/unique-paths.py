c206d8277747f9687bf39fded653da8f93b8c6f5
lakshh670 <er.lakshyasharma2005@gmail.com>
2026-08-13 18:34:22 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (74.34%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,5 +1,6 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
+        # This will give MLE
         # q=deque()
         # count=0
         # q.append((0,0))
@@ -13,24 +14,32 @@ class Solution:
         #             q.append((x+dx,y+dy))
         # return count
 
-        q=deque()
-        count=0
-        ways=[[0]*n for _ in range(m)]
-        ways[0][0]=1
-        q.append((0,0))
-        while q:
-            x,y=q.popleft()
-            if x+1<m:
-                if ways[x+1][y]==0:
-                    q.append((x+1,y))
-                ways[x+1][y]+=ways[x][y]
+        # q=deque()
+        # count=0
+        # ways=[[0]*n for _ in range(m)]
+        # ways[0][0]=1
+        # q.append((0,0))
+        # while q:
+        #     x,y=q.popleft()
+        #     if x+1<m:
+        #         if ways[x+1][y]==0:
+        #             q.append((x+1,y))
+        #         ways[x+1][y]+=ways[x][y]
                 
-            if y+1<n:
-                if ways[x][y+1]==0:
-                    q.append((x,y+1))
-                ways[x][y+1]+=ways[x][y]
+        #     if y+1<n:
+        #         if ways[x][y+1]==0:
+        #             q.append((x,y+1))
+        #         ways[x][y+1]+=ways[x][y]
                 
-        return ways[m-1][n-1]
+        # return ways[m-1][n-1]
+
+        a=m+n-2
+        b=m-1
+        def fact(x):
+            if x==1 or x==0:
+                return 1
+            return x*fact(x-1)
+        return fact(a)//(fact(a-b)*fact(b))
 
         
         
