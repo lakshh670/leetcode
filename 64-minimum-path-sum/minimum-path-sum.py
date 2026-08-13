a830715b02995cf3f9237a5eaa1b8bf4ba5bf70e
lakshh670 <er.lakshyasharma2005@gmail.com>
2026-08-13 20:00:25 +0530

    Time: 31 ms (17.26%) | Memory: 20.4 MB (93.22%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.py
+++ 64-minimum-path-sum/minimum-path-sum.py
@@ -13,13 +13,26 @@ class Solution:
         #     return dp[i][j]
         # return solve(0,0)
 
-        dp=[[float('inf')]*(n+1) for _ in range(m+1)]
-        dp[m-1][n-1]=grid[m-1][n-1]
+        # dp=[[float('inf')]*(n+1) for _ in range(m+1)]
+        # dp[m-1][n-1]=grid[m-1][n-1]
+        # for i in range(m-1,-1,-1):
+        #     for j in range(n-1,-1,-1):
+        #         if i==m-1 and j==n-1:
+        #             continue
+        #         dp[i][j]=grid[i][j]+min(dp[i+1][j],dp[i][j+1])
+        # return dp[0][0]
+
+        forw=[float('inf')]*(n+1) 
+        
         for i in range(m-1,-1,-1):
+            curr=[float('inf')]*(n+1)
             for j in range(n-1,-1,-1):
                 if i==m-1 and j==n-1:
+                    curr[j]=grid[i][j]
                     continue
-                dp[i][j]=grid[i][j]+min(dp[i+1][j],dp[i][j+1])
-        return dp[0][0]
+                print(forw[j])
+                curr[j]=grid[i][j]+min(forw[j],curr[j+1])
+            forw=curr
+        return forw[0]
         
         
